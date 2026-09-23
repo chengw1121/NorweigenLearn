115b64d857512c5db72767133ee976a039353ce4
chengw1121 <chengw1121@users.noreply.github.com>
2026-09-23 12:02:56 +0200

    Redesign learning navigation and add future topic course

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "norsk-hver-dag-v7";
-const FILES = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "norsk-hver-dag-v8";
+const FILES = ["./", "./index.html", "./styles.css", "./app.js", "./topic-fremtid.js", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener("fetch", event => event.respondWith(
