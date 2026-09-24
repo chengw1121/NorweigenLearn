4f1988f356eb31439aa593ff5e52f46ce1f3f0a3
chengw1121 <chengw1121@users.noreply.github.com>
2026-09-24 22:25:40 +0200

    Improve reading word explanations and daily preferences

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "norsk-hver-dag-v127";
-const FILES = ["./", "./index.html", "./styles.css?v=14", "./app.js?v=15", "./topic-fremtid.js?v=10", "./reading-library.js?v=1", "./vocab-v2-library.js?v=114", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "norsk-hver-dag-v133";
+const FILES = ["./", "./index.html", "./styles.css?v=17", "./app.js?v=21", "./topic-fremtid.js?v=10", "./reading-library.js?v=1", "./vocab-v2-library.js?v=114", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener("fetch", event => {
